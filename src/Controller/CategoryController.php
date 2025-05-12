504e3119939a6f8c94f685bb0b65564a6299aef1
ISLEM FOURATI <fourati.islem@outlook.fr>
2025-05-12 07:14:25 +0200

    affichage des evenement liée à une catégorie , utilisation d'une route qui contient slug

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CategoryController extends AbstractController
+{
+    //ici on a ajouté un slug pour la route afin qu'on puisse aller sur la page 
+    #[Route('/category/{slug}', name: 'app_category')]
+
+
+    public function index($slug, CategoryRepository $categoryRepository): Response
+    {
+        //On injection la dépendance CategoryRepository : une fonction qui va aller à la bdd un objet qui contient ce slug
+        //1. je craie une connexion à la bdd
+        //2. se connecter à la table category
+        //3. faire une action sur la table category 
+
+        // $category = $categoryRepository->findAll(); //si on veut récupérer toutes les catégories
+        //$category = $categoryRepository->findOneBy(['slug' => $slug]); // ca marche aussi comme ca
+        $category = $categoryRepository->findOneBySlug($slug); // on va chercher la catégorie par son slug 
+
+
+        //dd($category); //dump and die : pour voir ce qu'il y a dans la variable $category
+
+        return $this->render('category/index.html.twig', [
+            'category' => $category,
+        ]);
+    }
+}
